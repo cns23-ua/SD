8fb88109bdb805419e9aa8c20d79b54db97cd618
lpv24 <lpv24@alu.ua.es>
2023-11-09 17:38:38 +0100

    Directorio ready para seguir trabajando

--- Groso/AD_Weather.py
+++ Groso/AD_Weather.py
@@ -1,21 +1,21 @@
 import socket
 
+
+HEADER = 64
+FORMAT = 'utf-8'
+SERVER = "127.0.0.4"
+
 class AD_Weather:
-    def __init__(self, puerto):
+    def __init__(self, puerto, ciudades):
         self.puerto = puerto
-        self.ciudades = {
-            "Madrid": 25,
-            "Londres": 20,
-            "París": 22,
-            # ... otras ciudades y sus temperaturas
-        }
+        self.ciudades = eval(ciudades)
 
     def obtener_temperatura(self, ciudad):
-        return self.ciudades.get(ciudad, "No disponible")
+        return self.ciudades[ciudad]
 
     def iniciar_servidor(self):
         servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        servidor.bind(('localhost', self.puerto))
+        servidor.bind((SERVER, self.puerto))
         servidor.listen()
 
         print(f"Servidor de clima escuchando en el puerto {self.puerto}")
@@ -24,14 +24,16 @@ class AD_Weather:
             conn, addr = servidor.accept()
             print(f"Conexión establecida desde {addr}")
 
-            ciudad = conn.recv(1024).decode('utf-8')
-
-            temperatura = self.obtener_temperatura(ciudad)
+            msg_length = conn.recv(HEADER).decode(FORMAT)
+            if msg_length:
+                msg_length = int(msg_length)
+                message = conn.recv(msg_length).decode(FORMAT)
+                
+                temperatura = self.obtener_temperatura(message)
 
             conn.send(str(temperatura).encode('utf-8'))
-            conn.close()
 
 if __name__ == "__main__":
     puerto = 10000  # Puerto de escucha
-    weather_server = AD_Weather(puerto)
+    weather_server = AD_Weather(puerto, "ciudades.json")
     weather_server.iniciar_servidor()
