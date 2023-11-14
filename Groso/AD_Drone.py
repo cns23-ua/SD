b37e44b4859b6a099fe49203428ab71d2b7b5a7b
cns23-ua <cns23-ua>
2023-11-14 20:12:17 +0100

    timeout de si se cae el engine + laure se cae drones merged

--- Groso/AD_Drone.py
+++ Groso/AD_Drone.py
@@ -77,23 +77,25 @@ class Dron:
     # !Kafka:
     
     # * Funcion que recibe el destino del dron mediante kafka
-    def recibir_destino(self, servidor_kafka, puerto_kafka):
-        consumer = KafkaConsumer(bootstrap_servers= servidor_kafka + ":" + str(puerto_kafka))
-
+    def recibir_destino_con_timeout(self, servidor_kafka, puerto_kafka, timeout_segundos, cliente):
+        consumer = KafkaConsumer(bootstrap_servers=f"{servidor_kafka}:{puerto_kafka}")
         topic = "destinos_a_drones_topic"
-        
         consumer.subscribe([topic])
-        
-        for msg in consumer:
-            if msg.value:
-                mensaje = loads(msg.value.decode('utf-8'))
-               
+
+        try:
+            msg = consumer.poll(timeout_ms=timeout_segundos * 1000)
+            if msg:
+                mensaje = loads(next(iter(msg.values()))[0].value.decode('utf-8'))
                 self.destino = eval(mensaje)[self.id]
-                
-                x = int(self.destino.split(",")[0])
-                y = int(self.destino.split(",")[1])
-                self.destino = Coordenada(x,y)
-                break  # Sale del bucle al recibir un mensaje exitoso
+                x, y = map(int, self.destino.split(","))
+                self.destino = Coordenada(x, y)
+            else:
+                print("Error: No se pudo recibir el destino. El engine no está operativo.")
+                cliente.close()
+
+        except KafkaTimeoutError:
+            print("Error: Se agotó el tiempo de espera. El engine no está operativo.")
+            cliente.close()
 
     # * Función para recibir el mapa
     def recibir_mapa(self, servidor_kafka, puerto_kafka):
@@ -306,7 +308,7 @@ class Dron:
 
                 while True:
                     try:
-                        self.recibir_destino("127.0.0.1", 9092)
+                        self.recibir_destino_con_timeout("127.0.0.1", 9092,5)
                         mapa_actualizado_cuadros = self.recibir_mapa("127.0.0.1", 9092)
                         if mapa_actualizado_cuadros != self.mapa.cuadros:
                             self.mapa.cuadros = mapa_actualizado_cuadros
